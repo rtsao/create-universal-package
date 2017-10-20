b49a3e80148728edde9d77dc0c03200829573625
Leo Horie <lhorie@uber.com>
2017-10-19 18:05:23 -0700

    use the real thing now that it's published

--- packages/create-universal-package/lib/config/test-browser.js
+++ packages/create-universal-package/lib/config/test-browser.js
@@ -1,7 +1,7 @@
 const babel = require('rollup-plugin-babel');
 const multiEntry = require('rollup-plugin-multi-entry');
 const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('@lhorie/rollup-plugin-commonjs');
+const commonjs = require('rollup-plugin-commonjs');
 const replace = require('rollup-plugin-replace');
 const browserConfig = require('./browser-config.js');
 
@@ -11,9 +11,9 @@ module.exports = (userBabelConfig) => ({
   formats: ['iife'],
   template,
   plugins: [
-    multiEntry({exports: false}),
     resolve({browser: true}),
     commonjs({include: 'node_modules/**'}),
+    multiEntry({exports: false}),
     babel(Object.assign({}, browserConfig(userBabelConfig), {exclude: 'node_modules/**'})),
     replace({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
