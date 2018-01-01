b47f64c5c0967211d0d77fef1b5dc248db7e9f6e
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-01-01 15:57:30 -0800

    Ensure dist is created before gen flow libdef

--- packages/create-universal-package/lib/tasks/gen-flow-libdef.js
+++ packages/create-universal-package/lib/tasks/gen-flow-libdef.js
@@ -1,7 +1,15 @@
 const {promisify} = require('util');
 const execFile = promisify(require('child_process').execFile);
 const flow = require('flow-bin');
+const mkdir = promisify(require('fs').mkdir);
 
 module.exports = async function genFlowLibdef(filePath, outDir) {
+  try {
+    await mkdir(outDir);
+  } catch (e) {
+    if (e.code !== 'EEXIST') {
+      throw e;
+    }
+  }
   return execFile(flow, ['gen-flow-files', filePath, '--out-dir', outDir]);
 };
